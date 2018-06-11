31126450199bbbbd814e5725b35c5b1826fbd674
huxuelei <huxuelei@beisen.com>
2018-06-11 19:15:17 +0800

    资料开始

---
+++ client/pages/information/index.js
@@ -0,0 +1,78 @@
+// pages/information/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    pagTitle:"资料",//页面标题名称
+    array:['全部','a','b','b'],
+    showSelection:false,
+    showSearchSelection:function(){
+      var that = this;
+      alert(887)
+      this.setData({
+        showSelection: !that.data.showSelection
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.setNavigationBarTitle({
+      title: that.data.pagTitle
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
