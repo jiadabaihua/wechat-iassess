f3dfd371b2e58cb23926267e32dce1dc72143234
huxuelei <huxuelei@beisen.com>
2018-08-19 19:08:41 +0800

    d

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -45,7 +45,7 @@ Page({
       })
       
       wx.request({
-        url: 'http://dyhzjbeisen.ceping.com/Activity/GetActivityList ', //仅为示例，并非真实的接口地址
+        url: 'http://dyhzjbeisen.ceping.com.cn/Activity/GetActivityList ', //仅为示例，并非真实的接口地址
         data: app.globalData.dataList,
         header: {
           'content-type': 'application/json' // 默认值
